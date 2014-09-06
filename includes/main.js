c70537737029bfded96555d62544eb67157965d1
Aulus Diniz <Aulus999@gmail.com>
2014-09-06 12:53:38 -0300

    Correcao de bug que nao permitia clicar na carta pela sobre posicao da div que carrega as perguntas para o usuario; Organizacao e indentacao de CSS.

--- includes/main.js
+++ includes/main.js
@@ -314,7 +314,7 @@ window.onload = function() {
 											
 											//publishTrinca();
 											$("#popup_window").load("sample.html");
-											$("#popup_window").css( "visibility", "true" );
+											$("#popup_window").css( "display", "inline" );
 											$("#popup_window").css( "background-color", "#09C" );
 											
 										}
